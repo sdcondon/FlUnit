883a1697615c429fc9116e1dbb2f22fba468f775
Simon Condon <simoncondon@hotmail.com>
2020-02-16 10:33:01 +0000

    Added some very basic unit tests.

---
+++ src/FlUnit._Tests/TestThatTests.cs
@@ -0,0 +1,107 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Linq;
+
+namespace FlUnit._Tests
+{
+    [TestClass]
+    public class TestThatTests
+    {
+        [TestMethod]
+        public void ActAndAssert_WithMinimalTest()
+        {
+            // Arrange
+            var test = TestThat
+                .When(() => { })
+                .Then(a => { }, "Empty assertion");
+
+            // Act & Assert
+            ((Action)test.Act).ShouldNotThrow();
+            test.Assertions.Count().ShouldBe(1);
+
+            var assertion = test.Assertions.Single();
+            assertion.Description.ShouldBe("Empty assertion");
+            ((Action)assertion.Invoke).ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void ActAndAssert_WithSimpleTest()
+        {
+            // Arrange
+            var test = TestThat
+                .Given(new { x = 1, y = 1 })
+                .When(given => given.x + given.y)
+                .Then((given, sum) => sum.Result.ShouldBe(2));
+
+            // Act & Assert
+            ((Action)test.Act).ShouldNotThrow();
+            test.Assertions.Count().ShouldBe(1);
+
+            var assertion = test.Assertions.Single();
+            assertion.Description.ShouldBe("sum.Result.ShouldBe(2)");
+            ((Action)assertion.Invoke).ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void ActAndAssert_WithMultiplePrereqsAndAssertions()
+        {
+            // Arrange
+            var test = TestThat
+                .Given(1)
+                .And(1)
+                .When((x, y) => x + y)
+                .Then((x, y, sum) => sum.Result.ShouldBeGreaterThan(x))
+                .And((x, y, sum) => sum.Result.ShouldBeGreaterThan(y));
+
+            // Act & Assert
+            ((Action)test.Act).ShouldNotThrow();
+            test.Assertions.Count().ShouldBe(2);
+
+            var assertion1 = test.Assertions.First();
+            assertion1.Description.ShouldBe("sum.Result.ShouldBeGreaterThan(x)");
+            ((Action)assertion1.Invoke).ShouldNotThrow();
+
+            var assertion2 = test.Assertions.Skip(1).First();
+            assertion2.Description.ShouldBe("sum.Result.ShouldBeGreaterThan(y)");
+            ((Action)assertion2.Invoke).ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void ActAndAssert_WithExceptionExpected()
+        {
+            // Arrange
+            var test = TestThat
+                .Given(new { x = 1, y = 0 })
+                .When(given => given.x / given.y)
+                .Then((given, division) => division.Exception.GetBaseException().ShouldBeOfType(typeof(DivideByZeroException)));
+                //.Then((given, division) => division.Exception.ShouldBeOfType(typeof(DivideByZeroException))); // Don't use Task..
+
+            // Act & Assert
+            ((Action)test.Act).ShouldNotThrow();
+            test.Assertions.Count().ShouldBe(1);
+
+            var assertion = test.Assertions.Single();
+            assertion.Description.ShouldBe("division.Exception.GetBaseException().ShouldBeOfType(System.DivideByZeroException)");
+            ((Action)assertion.Invoke).ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void ActAndAssert_WithFailingAssertion()
+        {
+            // Arrange
+            var test = TestThat
+                .Given(new { x = 1, y = 1 })
+                .When(given => given.x + given.y)
+                .Then((given, sum) => sum.Result.ShouldBe(3));
+
+            // Act & Assert
+            ((Action)test.Act).ShouldNotThrow();
+            test.Assertions.Count().ShouldBe(1);
+
+            var assertion = test.Assertions.Single();
+            assertion.Description.ShouldBe("sum.Result.ShouldBe(3)");
+            ((Action)assertion.Invoke).ShouldThrow(typeof(ShouldAssertException));
+        }
+    }
+}
