3bcdcc4850ec2f1054ec7e8e29dc3f11af70f613
Simon Condon <simoncondon@hotmail.com>
2022-02-04 22:20:27 +0000

    Static analysis message fixes.

--- src/FlUnit/TestThat.cs
+++ src/FlUnit/TestThat.cs
@@ -22,7 +22,7 @@ namespace FlUnit
         /// <typeparam name="T">The type of the pre-requisite.</typeparam>
         /// <param name="prerequisite">A delegate to create the pre-requisite.</param>
         /// <returns>A builder for providing more "Given" clauses or a "When" clause.</returns>
-        public static TestBuilderWithPrerequisites<T> Given<T>(Func<T> prerequisite) => new TestBuilderWithPrerequisites<T>(new Action<ITestConfiguration>[] { }, () => new[] { prerequisite() });
+        public static TestBuilderWithPrerequisites<T> Given<T>(Func<T> prerequisite) => new TestBuilderWithPrerequisites<T>(Array.Empty<Action<ITestConfiguration>>(), () => new[] { prerequisite() });
 
         /// <summary>
         /// Starts building a test with multiple cases by providing a "Given" clause.
@@ -30,20 +30,20 @@ namespace FlUnit
         /// <typeparam name="T">The type of the pre-requisite.</typeparam>
         /// <param name="prerequisites">A delegate to create the pre-requisites - one for each test case.</param>
         /// <returns>A builder for providing more "Given" clauses or a "When" clause.</returns>
-        public static TestBuilderWithPrerequisites<T> GivenEachOf<T>(Func<IEnumerable<T>> prerequisites) => new TestBuilderWithPrerequisites<T>(new Action<ITestConfiguration>[] { }, prerequisites);
+        public static TestBuilderWithPrerequisites<T> GivenEachOf<T>(Func<IEnumerable<T>> prerequisites) => new TestBuilderWithPrerequisites<T>(Array.Empty<Action<ITestConfiguration>>(), prerequisites);
 
         /// <summary>
         /// Starts building a test with no "Given" clauses and a "When" clause that does not return a value.
         /// </summary>
         /// <param name="testAction">The action that is the "When" clause of the test.</param>
         /// <returns>A builder for providing "Then" clauses.</returns>
-        public static TestBuilderWithAction When(Action testAction) => new TestBuilderWithAction(new Action<ITestConfiguration>[] { }, testAction);
+        public static TestBuilderWithAction When(Action testAction) => new TestBuilderWithAction(Array.Empty<Action<ITestConfiguration>>(), testAction);
 
         /// <summary>
         /// Starts building a test with no "Given" clauses and a "When" clause that returns a value.
         /// </summary>
         /// <param name="testFunction">The function that is the "When" clause of the test.</param>
         /// <returns>A builder for providing "Then" clauses.</returns>
-        public static TestBuilderWithFunction<T> When<T>(Func<T> testFunction) => new TestBuilderWithFunction<T>(new Action<ITestConfiguration>[] { }, testFunction);
+        public static TestBuilderWithFunction<T> When<T>(Func<T> testFunction) => new TestBuilderWithFunction<T>(Array.Empty<Action<ITestConfiguration>>(), testFunction);
     }
 }
